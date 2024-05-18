643511a26982a7dd635b3de562af05c481c0e199
Lane Wilson <lane.m.wilson00@gmail.com>
2024-05-18 22:51:18 +0200

    initial commit

---
+++ src/main/java/com/SignicatTask/SignicatTask/SignicatTaskApplication.java
@@ -0,0 +1,13 @@
+package com.SignicatTask.SignicatTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignicatTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignicatTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SignicatTask/SignicatTask/SignicatTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SignicatTask.SignicatTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignicatTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
