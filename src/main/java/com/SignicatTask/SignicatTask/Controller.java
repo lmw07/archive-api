003a2f8c53105c41ca5e5fcee87e4d34dae8d434
Lane Wilson <lane.m.wilson00@gmail.com>
2024-05-19 23:01:08 +0200

    Rewrote Controller to be synchronous. Passes basic tests

--- src/main/java/com/SignicatTask/SignicatTask/Controller.java
+++ src/main/java/com/SignicatTask/SignicatTask/Controller.java
@@ -1,29 +1,80 @@
 package com.SignicatTask.SignicatTask;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.SignicatTask.SignicatTask.Archiving.ArchiveService;
 import com.SignicatTask.SignicatTask.Repository.LogRepository;
+import com.SignicatTask.SignicatTask.Repository.RequestData;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.time.LocalDate;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class Controller {
 
-    private final LogRepository repo;
+    private final LogRepository logRepo;
+    private ArchiveService archiveService;
 
-    public Controller(LogRepository repository){
-        this.repo = repository;
+    public Controller(LogRepository repository, ArchiveService archiveService){
+        this.logRepo = repository;
+        this.archiveService = archiveService;
     }
+
 
     @PostMapping("/upload")
-    //TODO
-    public String processFileUpload(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
-    }
-    
+    public ResponseEntity<?> uploadFiles(@RequestParam("files") MultipartFile[] files, HttpServletRequest request) {
+        if (files.length == 0 || files == null) {
+            return ResponseEntity.badRequest().build();
+        }
 
+        // Allow no file larger than 1MB
+        final long MAX_FILE_SIZE = 1024*1024;
+        final long MAX_TOTAL_SIZE = MAX_FILE_SIZE * 10;
+
+        //check size of files
+        long totalSize = 0;
+        for (MultipartFile file : files) {
+            long fileSizeInBytes = file.getSize();
+            totalSize += fileSizeInBytes;
+            if (fileSizeInBytes > MAX_FILE_SIZE || totalSize > MAX_TOTAL_SIZE) {
+                return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("Files larger than 1MB not allowed.");
+                
+            }
+        }
+
+
+        try {
+            byte[] zipFile = archiveService.archiveFiles(files);
+            
+            // save to DB
+            RequestData rqd = new RequestData(LocalDate.now(), request.getRemoteAddr(), RequestData.Status.SUCCESS);
+            logRepo.save(rqd);
+
+            return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"archive.zip\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(zipFile);
+        } catch (Exception e) {
+            // Log the error and return an appropriate error response
+            return ResponseEntity.internalServerError()
+                .body(null);
+        }
+    }
 }
+
+
+
+
+
